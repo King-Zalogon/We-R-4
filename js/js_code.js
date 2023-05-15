ce9ce9cf321d8047e6105fd73654fead3f7dd643
santiagoRoda <santiroda04@gmail.com>
2023-05-14 23:50:00 -0300

    agregue estilos a los carteles y arrelgle un poco la responisividad

---
+++ js/js_code.js
@@ -0,0 +1,30 @@
+let provincias = ["Buenos Aires",
+"Catamarca",
+"Chaco",
+"Chubut",
+"Córdoba",
+"Corrientes",
+"Entre Ríos",
+"Formosa",
+"Jujuy",
+"La Pampa",
+"La Rioja",
+"Mendoza",
+"Misiones",
+"Neuquén",
+"Río Negro",
+"Salta",
+"San Juan",
+"San Luis",
+"Santa Cruz",
+"Santa Fe",
+"Santiago del Estero",
+"Tierra del Fuego",
+"Tucumán"]
+
+
+let selectorDeProvincias = document.getElementById("provincias")
+
+console.log(selectorDeProvincias)
+for (let i of provincias){
+}
