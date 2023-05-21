a98a2bf9e34b22a3123c321121de1c8c0c6c966b
santiagoRoda <santiroda04@gmail.com>
2023-05-20 21:26:09 -0300

    Agregue la api con Vue

--- js/js_code.js
+++ js/js_code.js
@@ -29,3 +29,51 @@ console.log(selectorDeProvincias)
 for (let i of provincias){
     selectorDeProvincias.innerHTML += `<option>${i}</option>`
 }
+
+const {createApp} = Vue 
+const miApp = createApp({
+    
+    data(){
+        return{
+            main: document.querySelector(".perfiles")
+        }
+    },
+    mounted(){
+        this.imagen = "imagen1.png",
+        this.altura = 50
+        this.main = document.querySelector(".perfiles")
+    },
+
+    methods:{
+        verMas(){
+            for(let i = 0; i <= 2; i++){
+                this.generarPerfil()
+            }
+
+            
+        },
+
+        generarPerfil(){
+            fetch("https://randomuser.me/api/")
+            .then(data => data.json())
+            .then(data => data)
+            .then(data =>{
+                console.log(data)
+                this.main.innerHTML += `<div class="perfil">
+            <img src="${data["results"][0]["picture"]["large"]}" alt="" class="perfil-img">
+            <div class="datos">
+                <h4>${data["results"][0]["name"]["first"]} ${data["results"][0]["name"]["last"]}</h4>
+                <p class="descripcion">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur alias quas ea aperiam, soluta sint officiis ut? Eum aut nihil minima, suscipit, facere pariatur consequatur dolorum sequi ipsam neque sapiente illum sit aliquam deserunt veniam non, alias voluptate laboriosam ipsa odit quidem est? Laudantium adipisci, cum vero assumenda tempora placeat.</p>
+                <p class="modalidad"><b>Modalidad:</b> A distancia/presencial</p>
+                <p class="zona"><b>Zona:</b> C.A.B.A</p>
+                <p class="telefono"><b>Telefono:</b> 11 56369556</p>
+            </div>
+            
+        </div>`
+            })
+            
+        }
+    }
+
+})
+miApp.mount("#main__app")
