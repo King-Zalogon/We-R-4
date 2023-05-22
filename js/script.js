5e96c48d220e12964ab0f6fece064d3fd98857aa
ZalogonKing <gonzaloreydelcastillo@gmail.com>
2023-05-21 21:33:27 -0300

    Agrego formulario de contacto
    Usa bootstrap y jscript para validar.
    Tambien cambié el "Iniciar Sesión" que no lleva a ningun lado hasta que tengamos backend por "Contactanos"

---
+++ js/script.js
@@ -0,0 +1,56 @@
+new Vue({
+    el: '#app',
+    data: {
+      name: '',
+      email: '',
+      subject: '',
+      message: '',
+      showConfirmation: false
+    },
+    methods: {
+      submitForm() {
+        if (this.validateForm()) {
+          // Send form data to email (replace with your email sending logic)
+          // Example using JavaScript fetch API:
+          fetch('/send-email', {
+            method: 'POST',
+            body: JSON.stringify({
+              name: this.name,
+              email: this.email,
+              subject: this.subject,
+              message: this.message
+            }),
+            headers: {
+              'Content-Type': 'application/json'
+            }
+          })
+          .then(response => {
+            if (response.ok) {
+              this.showConfirmation = true;
+            }
+          })
+          .catch(error => {
+            console.error('Error:', error);
+          });
+        }
+      },
+      validateForm() {
+        const form = document.querySelector('.needs-validation');
+        if (form.checkValidity() === false) {
+          form.classList.add('was-validated');
+          return false;
+        }
+        return true;
+      },
+      resetForm() {
+        this.name = '';
+        this.email = '';
+        this.subject = '';
+        this.message = '';
+        this.showConfirmation = false;
+        const form = document.querySelector('.needs-validation');
+        form.classList.remove('was-validated');
+      }
+    }
+  });
+  
