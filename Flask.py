451f20918ea4f6ae1d85f04020a0436b68ef4fd9
santiagoRoda <santiroda04@gmail.com>
2023-07-09 17:35:51 -0300

    Agregado de Api con Flask y modificacion del formulario

--- Flask.py
+++ Flask.py
@@ -21,10 +21,12 @@ def crear_tabla():
             nombre VARCHAR(25) NOT NULL,
             apellido VARCHAR(25) NOT NULL,
             correoElectronico VARCHAR(30) NOT NULL,
+            telefono INTEGER(14) NOT NULL,
             fechaNac VARCHAR(12) NOT NULL,
-            provincia VARCHAR(30) NOT NULL,
+            provincia VARCHAR(30),
             descripcion VARCHAR(360) NOT NULL,
             materia VARCHAR(30), 
+            modalidad VARCHAR(30),
             foto TEXT NOT NULL 
         )
     ''')
@@ -45,22 +47,25 @@ crear_db()
 
 class Profesor:
     
-    def __init__(self,nombre,apellido,email,nacimiento,provincia,descripcion,materia,foto):
+    def __init__(self,nombre,apellido,email,telefono,nacimiento,provincia,descripcion,materia,modalidad,foto):
         self.id =  None
         self.nombre = nombre
         self.apellido = apellido
         self.email = email
+        self.telefono = telefono
         self.nacimiento = nacimiento
         self.provincia = provincia
         self.descripcion = descripcion
         self.materia = materia 
+        self.modalidad = modalidad
         self.foto = foto
         
-    def modificar(self,nueva_provincia,nueva_descripcion,nueva_materia,nueva_foto):
+    def modificar(self,nueva_provincia,nueva_descripcion,nueva_materia,nueva_modalidad,nueva_foto):
         self.provincia = nueva_provincia
         self.descripcion = nueva_descripcion
         self.materia = nueva_materia
         self.foto = nueva_foto
+        self.modalidad = nueva_modalidad
         
 class Profesores:
     def __init__(self):
@@ -75,28 +80,28 @@ class Profesores:
             return row
         return False
     
-    def agregar(self,nombre,apellido,email,nacimiento,provincia,descripcion,materia,foto):
+    def agregar(self,nombre,apellido,email,telefono,nacimiento,provincia,descripcion,materia,modalidad,foto):
         conect = conectar()
         cursor = conect.cursor()
         profesor_existente = self.consultar_profe(email)
         if profesor_existente:
             return jsonify({"mensaje":"Este email ya se encuentra registrado"})
         
-        nuevo_profesor = Profesor(nombre,apellido,email,nacimiento,provincia,descripcion,materia,foto)
-        cursor.execute("""INSERT INTO profesores VALUES (?,?,?,?,?,?,?,?)""",
-                           (nombre,apellido,email,nacimiento,provincia,descripcion,materia,foto))
+        nuevo_profesor = Profesor(nombre,apellido,email,telefono,nacimiento,provincia,descripcion,materia,modalidad,foto)
+        cursor.execute("""INSERT INTO profesores VALUES (?,?,?,?,?,?,?,?,?,?)""",
+                           (nombre,apellido,email,telefono,nacimiento,provincia,descripcion,materia,modalidad,foto))
         conect.commit()
         
         return jsonify({"mensaje":"El registro se ha completado con éxito"}),200
     
-    def modificar_profe(self,email,nueva_provincia,nueva_descripcion,nueva_materia,nueva_foto):
+    def modificar_profe(self,email,nueva_provincia,nueva_descripcion,nueva_materia,nueva_modalidad,nueva_foto):
         conect = conectar()
         cursor = conect.cursor()
         profesor_existente = self.consultar_profe(email)
         
         if profesor_existente:
-            profesor_existente.modificar(nueva_provincia,nueva_descripcion,nueva_materia,nueva_foto)
-            cursor.execute("UPDATE profesores SET provincia = ?, descripcion = ?, materia = ?, foto = ? WHERE email = ?",(nueva_provincia,nueva_descripcion,nueva_materia,nueva_foto,email))
+            profesor_existente.modificar(nueva_provincia,nueva_descripcion,nueva_materia,nueva_modalidad,nueva_foto)
+            cursor.execute("UPDATE profesores SET provincia = ?, descripcion = ?, materia = ?, modalidad = ?, foto = ? WHERE email = ?",(nueva_provincia,nueva_descripcion,nueva_materia,nueva_modalidad,nueva_foto,email))
             conect.commit()
             return{"mensaje":"Perfil modificado correctamente"},200
         
@@ -110,8 +115,8 @@ class Profesores:
         profesores = []
         
         for fila in filas:
-            nombre,apellido,email,nacimiento,provincia,descripcion,materia,foto = fila
-            profesor = {"nombre":nombre,"apellido":apellido,"email":email,"nacimiento":nacimiento,"provincia":provincia,"descripcion":descripcion,"materia":materia,"foto":foto}
+            nombre,apellido,email,telefono,nacimiento,provincia,descripcion,materia,modalidad,foto = fila
+            profesor = {"nombre":nombre,"apellido":apellido,"email":email,"telefono":telefono,"nacimiento":nacimiento,"provincia":provincia,"descripcion":descripcion,"materia":materia,"modalidad":modalidad,"foto":foto}
             profesores.append(profesor)
         
         return jsonify(profesores),200
@@ -144,10 +149,12 @@ def agregar_profesor():
     email = request.json.get("email")
     nombre = request.json.get("nombre")
     apellido = request.json.get("apelllido")
+    telefono = request.json.get("telefono")
     nacimiento = request.json.get("nacimiento")
     provincia = request.json.get("provincia")
     descripcion = request.json.get("descripcion")
     materia = request.json.get("materia")
+    modalidad = request.json.get("modalidad")
     foto = request.json.get("foto")
     
     return profesores.agregar(nombre,apellido,email,nacimiento,provincia,descripcion,materia,foto)
@@ -157,17 +164,18 @@ def agregar_profesor():
 def mostrar_profesores():
     return profesores.listar_profesores()
     
-@app.route("/profesor/< str:email >",methods = ["PUT"])
+@app.route("/profesor/<string:email>",methods = ["PUT"])
 def editar_perfil(email):
     
     nueva_provincia = request.json.get("nueva_provincia")
     nueva_descripcion = request.json.get("nueva_descripcion")
     nueva_materia = request.json.get("nueva_materia")
+    nueva_modalidad = request.json.get("nueva_modalidad")
     nueva_foto = request.json.get("nueva_foto")
     
-    return profesores.modificar_profe(email,nueva_provincia,nueva_descripcion,nueva_materia,nueva_foto)
+    return profesores.modificar_profe(email,nueva_provincia,nueva_descripcion,nueva_materia,nueva_modalidad,nueva_foto)
 
-@app.route("/profesor/< str:email >", methods = ["DELETE"])
+@app.route("/profesor/<string:email>", methods = ["DELETE"])
 def eliminar(email):
     return profesores.eliminar_profesor(email)
 
--- js/valid_form.js
+++ js/valid_form.js
@@ -1,39 +1,141 @@
-function validarFormulario() {
-    let nombre = document.getElementById("user_first").value.trim();
-    let apellido = document.getElementById("user_last").value.trim();
-    let correo = document.getElementById("email_1").value;
-    let cumple = document.getElementById("birth").value;
-    let foto = document.getElementById("photo");
+let provincias = ["Buenos Aires",
+"Catamarca",
+"Chaco",
+"Chubut",
+"Córdoba",
+"Corrientes",
+"Entre Ríos",
+"Formosa",
+"Jujuy",
+"La Pampa",
+"La Rioja",
+"Mendoza",
+"Misiones",
+"Neuquén",
+"Río Negro",
+"Salta",
+"San Juan",
+"San Luis",
+"Santa Cruz",
+"Santa Fe",
+"Santiago del Estero",
+"Tierra del Fuego",
+"Tucumán"]
 
 
-    if (nombre === "" || apellido === "" || correo === "" || cumple === "") {
-        alert("Por favor, complete todos los campos del formulario.");
-        return false;
-     }
+let selectorDeProvincias = document.getElementById("provincias")
 
-     if (foto.value === "") {
-        alert("Por favor, seleccione un archivo para su foto de perfil.");
-        return false;
-     }
-     
-     const array = [nombre , apellido]
-     console.log(array[0])
-     console.log(array[1])
-     for (let x = 0; x < 2; x++){
-         for (let i = 0; i < array[x].length; i++) {
-            let charCode = array[x].charCodeAt(i);
-            if (!((charCode >= 65 && charCode <= 90) || (charCode >= 97 && charCode <= 122) || charCode === 32)) {
-               alert("Los campos 'Nombres' y 'Apellidos' solo puede contener caracteres alfabéticos y espacios.");
-               return false;
-            } 
+console.log(selectorDeProvincias)
+for (let i of provincias){
+   selectorDeProvincias.innerHTML += `<option>${i}</option>`
+}
+
+const { createApp } = Vue;
+const miApp = createApp({
+   data() {
+      return {
+         perfil: "",
+         materia: document.querySelector("#div_materia"),
+         modalidad : "",
+         provincia : document.getElementById("provincias")
+
+      };
+   },
+   mounted() {
+      this.materia = document.querySelector("#div_materia");
+      this.perfil = document.querySelector("#user_choice").value;
+      this.modalidad = document.querySelector("#modalidad").value;
+      this.provincia = document.getElementById("provincias");
+   },
+   methods: {
+      mostrar_materia() {
+         if (this.perfil == "profesor" || this.perfil == "ambas") {
+            this.materia.style.display = "block";
+         } else {
+            this.materia.style.display = "none";
+         }
+      },
+      mostrar_provincias(){
+         if (this.modalidad == "presencial" || this.modalidad == "ambas") {
+            this.provincia.style.display = "block";
+         } else {
+            this.provincia.style.display = "none";
+         }
+      },
+      validarFormulario(event) {
+         event.preventDefault()
+         let nombre = document.getElementById("user_first").value.trim();
+         let apellido = document.getElementById("user_last").value.trim();
+         let correo = document.getElementById("email_1").value.trim();
+         let numero = document.getElementById("numero").value.trim();
+         let cumple = document.getElementById("birth").value;
+         let descripcion = document.getElementById("descripcion").value;
+         let perfil = document.getElementById("user_perfil");
+         let modalidad = document.getElementById("modalidad");
+         let provincia = document.getElementById("provincias").value;
+         let foto = document.getElementById("photo");
+         if (nombre === "" || apellido === "" || correo === "" || numero === "" || cumple === ""|| descripcion === "" || perfil === "" || modalidad === "") {
+            alert("Por favor, complete todos los campos del formulario.");
+            return false;
+         }
+
+         try{
+            numero = parseInt(numero)
+            if (numero.length != 12){
+               alert("Numero de telofono invalido")
+               return False
+            }
+         }
+         catch{
+            alert("Numero de telofono invalido")
+         }
+
+         if (foto.value === "") {
+            alert("Por favor, ingrese una url con una imagen");
+            return false;
+         }
+         const array = [nombre, apellido];
+         console.log(array[0]);
+         console.log(array[1]);
+         for (let x = 0; x < 2; x++) {
+            for (let i = 0; i < array[x].length; i++) {
+               let charCode = array[x].charCodeAt(i);
+               if (
+                  !(
+                     (charCode >= 65 && charCode <= 90) ||
+                     (charCode >= 97 && charCode <= 122) ||
+                     charCode === 32
+                  )
+               ) {
+                  alert(
+                     "Los campos 'Nombres' y 'Apellidos' solo pueden contener caracteres alfabéticos y espacios."
+                  );
+                  return false;
+               }
+            
+            }
+
+         }
+
+         if (descripcion.length < 80){
+            alert("Ingresa una descripcion mas larga")
+            return false
+         }
+         else if(descripcion.length > 360){
+            alert("Ingresa una descripcion mas larga")
+            return false
+         }
+
+         let imagen = foto.value;
+         let extensiones = /(.jpg|.jpeg|.png|.gif)$/i;
+         if (!extensiones.exec(imagen)) {
+            alert(
+               "Extensión no admitida, por favor utilice extensiones .jpg/.jpeg/.png/.gif"
+            );
+            return false;
          }
       }
+   }
+});
 
-     let imagen = foto.value;
-     let extenciones = /(.jpg|.jpeg|.png|.gif)$/i;
-     if (!extenciones.exec(imagen)){
-        alert("Extensión no admitida, por favor utilice extensiones .jpg/.jpeg/.png/.gif");
-        return false;
-     }
-     
-}
+miApp.mount("#app");
